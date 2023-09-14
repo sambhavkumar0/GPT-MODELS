2edac1b524e11a90c6d3f20de7c4ee109cb1cdf3
SAMBHAV KUMAR MOHANTY <108188982+sambhavkumar0@users.noreply.github.com>
2023-09-14 17:37:19 +0530

    Skeleton

---
+++ WEBSITE1/script.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const calculateBtn = document.getElementById("calculate-btn");
+    const stateInput = document.getElementById("state");
+    const waterConsumptionInput = document.getElementById("water-consumption");
+    const showerTimeInput = document.getElementById("shower-time");
+    const dietDropdown = document.getElementById("diet");
+    const extraTasksInput = document.getElementById("extra-tasks");
+    const resultDiv = document.getElementById("result");
+
+    calculateBtn.addEventListener("click", function () {
+        const state = stateInput.value;
+        const waterConsumption = parseFloat(waterConsumptionInput.value) || 0;
+        const showerTime = parseFloat(showerTimeInput.value) || 0;
+        const diet = dietDropdown.value;
+        const extraTasks = extraTasksInput.value;
+
+        // Calculate the total water consumption
+        const totalWaterConsumption = waterConsumption + (showerTime * 12); // Assuming 12 liters per minute for showering
+
+        // Create a result message
+        const resultMessage = `
+            State: ${state}<br>
+            Total Water Consumption: ${totalWaterConsumption} liters per day<br>
+            Diet: ${diet}<br>
+            Extra Water-Using Tasks: ${extraTasks}
+        `;
+
+        // Display the result
+        resultDiv.innerHTML = resultMessage;
+    });
+});
+
+// Event listener for the Calculate button
+const calculateBtn = document.getElementById('calculate-btn');
+calculateBtn.addEventListener('click', updateResult);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const displayBtn = document.getElementById("display-btn");
+    const stateDropdown = document.getElementById("state-dropdown");
+    const selectedStateDiv = document.getElementById("selected-state");
+
+    displayBtn.addEventListener("click", function () {
+        const selectedValue = stateDropdown.value;
+        const selectedText = stateDropdown.options[stateDropdown.selectedIndex].text;
+
+        if (selectedValue !== "") {
+            selectedStateDiv.textContent = `Selected State: ${selectedText}, Counter Value: ${selectedValue}`;
+        } else {
+            selectedStateDiv.textContent = "Please select a state first.";
+        }
+    });
+});
+
+
+
+
+
